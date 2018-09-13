1f84ca472bd871140727700fed121d02aeb12786
Koosha Hosseiny <info@koosha.cc>
2018-09-13 14:29:27 +0430

    commit

--- asset/datex.js
+++ asset/datex.js
@@ -1,26 +1,24 @@
 /**
- * Attaches the calendar behavior to all required fields
+ * Attaches the calendar behavior to all date-popup-enabled fields
  */
 (function ($) {
-    var i, j, id, datePopup, info,
-        calendar, extend, lang, def,
-        max, min, findme, fmt, attr,
-        findmeName;
-
-    function popupify(dis, id) {
+    function attach(ctx) {
+        for (var id in Drupal.settings.datePopup) {
+            if (!Drupal.settings.datePopup.hasOwnProperty(id)) {
+                continue;
+            }
+            var data = Drupal.settings.datePopup[id];
+            if (data.func === 'datepicker') {
+                var find = $('#' + id);
+                window.glob = find;
+                console.log(find, 'find');
+                find.pDatepicker();
+            }
+        }
     }
 
     Drupal.behaviors.datex = {
-        attach: function (context) {
-            for (id in Drupal.settings.datePopup) {
-                var ddd = $('#' + id);
-                var data = Drupal.settings.datePopup[id];
-                if (data.func === 'datepicker') {
-                    console.log(ddd);
-                    ddd[0].pDatepicker();
-                }
-            }
-        }
+        attach: attach
     };
 
 })(jQuery);
